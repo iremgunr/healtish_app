0e96eb249962b578d7e76eef1d2a75c46684ae27
İrem Güner <37420789+iremgunr@users.noreply.github.com>
2021-04-17 02:21:33 +0300

    Add files via upload

---
+++ lib/model/motivation.dart
@@ -0,0 +1,26 @@
+class Motivation {
+  final String motivation;
+
+  Motivation({this.motivation});
+}
+
+final motivations = [
+  Motivation(motivation: 'Eat healthy = feel healthy'),
+  Motivation(motivation: 'Focus on Health, not weight'),
+  Motivation(motivation: 'Loving health reminder, love your body'),
+  Motivation(motivation: 'If you keep good food in your fridge, you will eat good'),
+  Motivation(motivation: 'Healthy food = Healthy life'),
+  Motivation(motivation: 'You are what you eat!'),
+  Motivation(motivation: 'Eat less sugar - you are sweet enough!'),
+  Motivation(motivation: 'Fall in love with taking care of your body'),
+  Motivation(motivation: 'Be the best version of you'),
+  Motivation(motivation: 'Create Healthy habits, not restrictions.'),
+  Motivation(motivation: 'A healthy outside starts from inside'),
+  Motivation(motivation: 'Your body deserves the best!'),
+  Motivation(motivation: 'Progress is impossible without change'),
+  Motivation(motivation: 'Eating well is a form of self-respect'),
+  Motivation(motivation: 'Be stronger than your excuse'),
+  Motivation(motivation: 'Your health is an investment, not an expense.'),
+  Motivation(motivation: 'To eat is a necessity, but to eat intelligently is an art.'),
+  Motivation(motivation: 'Food is fueli not therapy.'),
+];
